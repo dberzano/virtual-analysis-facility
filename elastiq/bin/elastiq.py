833ff3b5f308d05455178668286e3393ffdb9ec4
Dario Berzano <dario.berzano@cern.ch>
2013-09-23 17:40:41 +0200

    Elastiq working (but still many things hardcoded)

--- elastiq/bin/elastiq.py
+++ elastiq/bin/elastiq.py
@@ -12,10 +12,11 @@ import os
 
 
 configuration = {
-  'sleep_s': 1,
+  'sleep_s': 15,
   'waiting_jobs_threshold': 10,
-  'waiting_jobs_time_s': 30,
-  'n_jobs_per_vm': 4,
+  'waiting_jobs_time_s': 480,
+  'n_jobs_per_vm': 2,
+  'cmd_start': '/home/cloudcondor/cern-openstack/vmstart.sh'
 }
 do_main_loop = True
 
@@ -26,16 +27,19 @@ def exit_main_loop(signal, frame):
   do_main_loop = False
 
 
-def robust_cmd(params, max_attempts=20):
+def robust_cmd(params, max_attempts=20, suppress_stderr=True):
 
   shell = isinstance(params, basestring)
   sp = None
 
-  for n_attempts in range(0, max_attempts):
+  for n_attempts in range(1, max_attempts+1):
 
     try:
-      with open(os.devnull) as dev_null:
-        sp = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=dev_null, shell=shell)
+      if suppress_stderr:
+        with open(os.devnull) as dev_null:
+          sp = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=dev_null, shell=shell)
+      else:
+        sp = subprocess.Popen(params, stdout=subprocess.PIPE, shell=shell)
       sp.wait()
     except OSError:
       logging.error("Command cannot be executed: wait %ds before retrying..." % n_attempts)
@@ -60,6 +64,30 @@ def robust_cmd(params, max_attempts=20):
   else:
     logging.error("Giving up after %d attempts" % max_attempts)
     return None
+
+
+def scale_up(nvms):
+  """Invokes an external command to launch more Virtual Machines. The number of
+  VMs is specified as parameter.
+  Returns the number of launched VMs.
+  """
+
+  nvms = int(nvms)
+  n_succ = 0
+  n_fail = 0
+  logging.info("Launching %d new vm(s)..." % nvms)
+
+  for i in range(1, nvms+1):
+    #ret = robust_cmd([ 'echo', str(i), configuration['cmd_start'] ], suppress_stderr=False, max_attempts=2)
+    ret = robust_cmd([ configuration['cmd_start'] ], max_attempts=1)
+    if ret and 'output' in ret:
+      n_succ+=1
+      logging.info("VM launched OK. Requested: %d/%d | Success: %d | Failed: %d" % (i, nvms, n_succ, n_fail))
+    else:
+      n_fail+=1
+      logging.error("Launching VM failed. Requested: %d/%d | Success: %d | Failed: %d" % (i, nvms, n_succ, n_fail))
+
+  return n_succ
 
 
 def poll_condor_queue(): 
@@ -99,23 +127,29 @@ def main():
       if n_waiting_jobs > configuration['waiting_jobs_threshold']:
         if first_time_above_threshold != -1:
           if (check_time-first_time_above_threshold) > configuration['waiting_jobs_time_s']:
-            logging.info("WE ARE ABOVE THRESHOLD --> DO SOMETHING AND RESET!")
+            # Above threshold time-wise and jobs-wise: do something
+            logging.info("Waiting jobs: %d (above threshold of %d for more than %ds)" % \
+              (n_waiting_jobs, configuration['waiting_jobs_threshold'], configuration['waiting_jobs_time_s']))
+            scale_up( round(n_waiting_jobs / float(configuration['n_jobs_per_vm'])) )
             first_time_above_threshold = -1
           else:
-            logging.info("STAYING ABOVE THRESHOLD BUT NOT ENOUGH TIME...")
+            # Above threshold but not for enough time
+            logging.info("Waiting jobs: %d (still above threshold of %d for less than %ds)" % \
+              (n_waiting_jobs, configuration['waiting_jobs_threshold'], configuration['waiting_jobs_time_s']))
         else:
-          logging.info("FIRST TIME SEEN ABOVE THRESHOLD")
+          # First time seen above threshold
+          logging.info("Waiting jobs: %d (first time above threshold of %d)" % \
+            (n_waiting_jobs, configuration['waiting_jobs_threshold']))
           first_time_above_threshold = check_time
       else:
-        logging.info("NOT ABOVE THRESHOLD")
+        # Not above threshold: reset
+        logging.info("Waiting jobs: %d (below threshold of %d)" % \
+          (n_waiting_jobs, configuration['waiting_jobs_threshold']))
         first_time_above_threshold = -1
+    else:
+      logging.error("Cannot get the number of waiting jobs this time, sorry")
 
-
-    # check_time = time.time()
-    # if n_waiting_jobs != -1:
-    #   if check_time 
-
-    poll_condor_status()
+    #poll_condor_status()
 
     logging.info("Sleeping %d seconds" % configuration['sleep_s']);
     time.sleep( configuration['sleep_s'] )
