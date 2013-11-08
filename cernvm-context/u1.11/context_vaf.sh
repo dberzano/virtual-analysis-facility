63f92500fd023482218869efb6180fc25c14c67d
Dario Berzano <dario.berzano@cern.ch>
2013-11-08 11:08:38 +0100

    Context for uCernVM installs boto

--- cernvm-context/u1.11/context_vaf.sh
+++ cernvm-context/u1.11/context_vaf.sh
@@ -490,6 +490,47 @@ function ConfigYumProxy() {
   mv "$Conf".0 "$Conf"
 }
 
+# Install boto
+function ConfigBoto() {(
+
+  #
+  # Python 2.7 environment
+  #
+
+  # GCC
+  source /cvmfs/sft.cern.ch/lcg/external/gcc/4.7.2/x86_64-slc6-gcc47-opt/setup.sh ''  # empty arg needed!
+
+  # Python 2.7
+  export PythonPrefix=/cvmfs/sft.cern.ch/lcg/external/Python/2.7.3/x86_64-slc6-gcc47-opt
+  export PATH="$PythonPrefix/bin:$PATH"
+  export LD_LIBRARY_PATH="$PythonPrefix/lib:$LD_LIBRARY_PATH"
+
+  # Boto
+  export PyBotoPrefix='/var/lib/condor/boto'  # <-- will install boto there
+  export PATH="$PyBotoPrefix/bin:$PATH"
+  export LD_LIBRARY_PATH="$PyBotoPrefix/lib:$LD_LIBRARY_PATH"
+  export PYTHONPATH="$PyBotoPrefix/lib/python2.7/site-packages:$PYTHONPATH"
+
+  #
+  # Install boto
+  #
+
+  mkdir -p "$PyBotoPrefix/lib/python2.7/site-packages"
+  cd "$PyBotoPrefix"
+  mkdir src
+  cd src
+  git clone https://github.com/boto/boto.git .
+  python setup.py install --prefix $(cd ..;pwd)
+
+  #
+  # Merging Grid CA certificates in boto
+  #
+
+  cat /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/etc/grid-security/certificates/*.pem \
+    >> "$PyBotoPrefix"/lib/python2.7/site-packages/boto/cacerts/cacerts.txt
+
+)}
+
 # List of actions to perform
 function Actions() {
 
@@ -521,6 +562,7 @@ function Actions() {
 
   if [ $Master == 1 ] ; then
     Exec 'Configuring sshcertauth' ConfigSshcertauth "$VafConf_AuthMethod"
+    Exec 'Installing boto' -v ConfigBoto
     Exec 'Configuring elastiq' -v ConfigElastiq
   fi
 
