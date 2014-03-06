6ee15d57a269e2daa291c0435b66d1f34d3d99e5
Dario Berzano <dario.berzano@cern.ch>
2014-03-06 16:20:35 +0100

    Add default configuration file with RPM

--- elastiq/rpm/post-install.sh
+++ elastiq/rpm/post-install.sh
@@ -8,4 +8,39 @@ chmod u=rwx,g=rwx,o=x /var/lib/elastiq /var/log/elastiq
 chown elastiq:elastiq /var/lib/elastiq /var/log/elastiq
 ln -nfs /usr/bin/elastiq /etc/init.d/elastiq
 chkconfig --add elastiq
+
+if [ ! -e /etc/elastiq.conf ] ; then
+  cat > /etc/elastiq.conf <_EoF_
+[elastiq]
+sleep_s = 5
+check_queue_every_s = 15
+check_vms_every_s = 45
+waiting_jobs_threshold = 10
+waiting_jobs_time_s = 100
+n_jobs_per_vm = 4
+idle_for_time_s = 3600
+estimated_vm_deploy_time_s = 600
+
+[debug]
+#dry_run_shutdown_vms = 1
+#dry_run_boot_vms = 1
+
+[quota]
+min_vms = 0
+max_vms = 3
+
+[ec2]
+api_url = https://dummy.ec2.server/ec2/
+aws_access_key_id = my_username
+aws_secret_access_key = my_password
+image_id = ami-00000000
+api_version = 2013-02-01
+key_name =
+flavour =
+user_data_b64 =
+_EoF_
+fi
+chown elastiq:elastiq /etc/elastiq.conf
+chmod u=rw,g=rw,o= /etc/elastiq.conf
+
 exit 0
