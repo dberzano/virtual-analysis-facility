f81ecd0182787f65a7f290248c5bdc76978734a3
Dario Berzano <dario.berzano@cern.ch>
2014-03-13 11:39:01 +0100

    External version and RPM config-files

--- misc/create-rpm.sh
+++ misc/create-rpm.sh
@@ -13,32 +13,51 @@ export PackageDestDir="$PackageSourceDir/misc/dist"
 # Working directory must not be under source directory
 export PackageWorkDir="/tmp/vaf-work-dir"
 
-echo "Source: $PackageSourceDir"
-echo "Destination: $PackageDestDir"
+# Will be vendor and maintainer
+export Author='Dario Berzano <dario.berzano@cern.ch>'
 
-rm -rf "$PackageDestDir" "$PackageWorkDir"
+echo "> Source: $PackageSourceDir"
+echo "> Destination: $PackageDestDir"
+
+rm -rf "$PackageWorkDir"
 mkdir -p "$PackageDestDir" "$PackageWorkDir"
 
 fpm \
   -s dir \
   -t rpm \
-  --version '0.9.0' \
+  --force \
+  --version "$(cat VERSION)" \
   --name vaf-client \
   --package "$PackageDestDir" \
   --workdir "$PackageWorkDir" \
-  --vendor "Dario Berzano <dario.berzano@cern.ch>" \
-  --maintainer "Dario Berzano <dario.berzano@cern.ch>" \
+  --vendor "$Author" \
+  --maintainer "$Author" \
   --description 'Client for the Virtual Analysis Facility' \
   --url 'https://github.com/dberzano/virtual-analysis-facility' \
+  --config-files 'etc' \
   --prefix '/' \
   --exclude '.git' \
   --exclude '.gitignore' \
+  --exclude 'VERSION' \
   --exclude 'README.*' \
   --exclude 'misc' \
   --exclude 'tmp' \
-  .
+  . || exit 1
+
+# Cleanup
+rm -rf "$PackageWorkDir"
+
+# Package
+Rpm=$( ls -1rt "$PackageDestDir"/*.rpm | tail -n1 )
 
 echo '=== List of the packages directory ==='
 ls -l "$PackageDestDir"
-echo '=== RPM contents and information ==='
-rpm -qlip "$PackageDestDir"/*.rpm
+
+echo '=== RPM information ==='
+rpm -qip "$Rpm"
+
+echo '=== RPM contents ==='
+rpm -qlp "$Rpm"
+
+echo '=== RPM config files ==='
+rpm -qcp "$Rpm"
